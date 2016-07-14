ccedfebd96e938c42686ff1a57dbe54b35db28a4
Brian Nickerson <nickerson.brian@gmail.com>
2016-07-13 21:11:33 -0400

    Include the texter's message in the automated response.

--- server.rb
+++ server.rb
@@ -2,10 +2,11 @@ require 'sinatra'
 require 'twilio-ruby'
 
 post '/sms' do
-  puts "Message: #{params['Body']}!"
+  response = params['Body']
+  puts "Message: #{response}!"
 
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Added to our list of robot sightings! Head for the hills!"
+    r.Message "#{response} has been added to our list of robot sightings! Head for the hills!"
   end
   twiml.text
 end
